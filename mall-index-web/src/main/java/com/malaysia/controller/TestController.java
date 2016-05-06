f6d112d8f324a3840796a1ae4b068546179bca60
robotta <robotta@163.com>
2016-05-06 18:45:36 +0800

    添加测试

---
+++ mall-index-web/src/main/java/com/malaysia/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.malaysia.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2016/5/6.
+ */
+
+@Controller
+public class TestController {
+
+
+}
